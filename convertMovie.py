98ec11d0f58f63306c52041430bff8e29e62baa5
Niklas Sombert <niklas1b@yahoo.de>
2013-10-19 22:18:06 +0200

    Changes for Python version 3. And the best is: It works on 2.7, too!

--- convertMovie.py
+++ convertMovie.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
-print "Loading libraries..."
+print("Loading libraries...")
 from movies2hdd import Movies2HDD
 import sys
 import shutil
 movie = sys.argv[1]
-print "Moving "+movie+" to /tmp ..."
+print("Moving "+movie+" to /tmp ...")
 shutil.move(movie, "/tmp/")
-print "Converting it..."
+print("Converting it...")
 movie = movie.replace(".ts", "")
 Movies2HDD.convertMovie(Movies2HDD(), movie)
-print "Moving "+movie+".mpg back..."
+print("Moving "+movie+".mpg back...")
 shutil.move("/tmp/"+movie+".mpg", ".")
--- movies2hdd.py
+++ movies2hdd.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import ftplib
 import urllib
+try:
+	import urllib.request #Python 3
+except ImportError:
+	pass
 from xml.dom.minidom import parseString
 import subprocess
 import os
@@ -11,13 +15,13 @@ class Movies2HDD:
 
 	def connect(self, host, user, pwd):
 		self.conn = ftplib.FTP()
-		print(self.conn.connect(host))
-		print(self.conn.login(user, pwd))
-		print(self.conn.cwd("/media/hdd/movie"))
+		print((self.conn.connect(host)))
+		print((self.conn.login(user, pwd)))
+		print((self.conn.cwd("/media/hdd/movie")))
 		#return conn
 
 	def disconnect(self):
-		print(self.conn.quit())
+		print((self.conn.quit()))
 		return
 
 	def getAviableMovies(self,search):
@@ -43,13 +47,21 @@ class Movies2HDD:
 	def getPositionOfEpisode(self, series, episode):
 		lang = "de" #german
 		#search doesn't always work
-		sid = parseString(urllib.urlopen("http://thetvdb.com/api/GetSeries.php?seriesname="+series).read()).getElementsByTagName("Data")[0].getElementsByTagName("Series")[0].getElementsByTagName("seriesid")[0].firstChild.data #seriesid
+		try:
+			apianswer = urllib.urlopen("http://thetvdb.com/api/GetSeries.php?seriesname="+series).read()
+		except AttributeError:
+			apianswer = urllib.request.urlopen("http://thetvdb.com/api/GetSeries.php?seriesname="+series).read()
+
+		sid = parseString(apianswer).getElementsByTagName("Data")[0].getElementsByTagName("Series")[0].getElementsByTagName("seriesid")[0].firstChild.data #seriesid
 		if series == "Castle":
 			sid = 83462
-	#	txt = urllib.urlopen("http://thetvdb.com/api/FE84E205C6E3D916/series/"+str(sid)+"/all/"+lang+".xml").read()
+		try:
+			apianswer = urllib.urlopen("http://thetvdb.com/api/FE84E205C6E3D916/series/"+str(sid)+"/all/"+lang+".xml").read()
+		except AttributeError:
+			apianswer = urllib.request.urlopen("http://thetvdb.com/api/FE84E205C6E3D916/series/"+str(sid)+"/all/"+lang+".xml").read()
 	#	dom = parseString(txt)
 	#	dom = dom.getElementsByTagName("Data")[0]
-		dom = parseString(urllib.urlopen("http://thetvdb.com/api/FE84E205C6E3D916/series/"+str(sid)+"/all/"+lang+".xml").read()).getElementsByTagName("Data")[0]
+		dom = parseString(apianswer).getElementsByTagName("Data")[0]
 		episodes = dom.getElementsByTagName("Episode")
 		season = 0
 		for x in episodes:
@@ -66,12 +78,12 @@ class Movies2HDD:
 	def downloadMovie(self, movie):
 		file = open("/tmp/"+movie+".ts", "wb")
 		result = self.conn.retrbinary("RETR "+movie+".ts", file.write, 8*1024) #perhaps implement threading ;-)
-		print result
+		print(result)
 		file.close()
 		if result.startswith("2") == False:
 			raise BaseException
 		else:
-			print "TODO"
+			print("TODO")
 			#print(self.conn.delete(movie)) #TODO
 
 	def convertMovie(self, movie):
@@ -80,28 +92,28 @@ class Movies2HDD:
 		files = os.listdir("/tmp")
 		contents = list()
 		for x in files:
-			print "There is "+x+"."
+			print("There is "+x+".")
 			if x.startswith(movie):
-				print "It belongs to us! "+x
+				print("It belongs to us! "+x)
 				#if x.find("[") == -1 and x.find("_log") == -1 and x.find(".ts") == -1:
 					#contents.append(x[x.index("."):x.count("")-1])
 				if x.endswith(".ac3"):
 					contents.append(".ac3")
-					print x+" is in."
+					print(x+" is in.")
 				elif x.endswith(".mp2") and x.find("[") == -1:
 					contents.append(".mp2")
-					print x+" is in."
+					print(x+" is in.")
 				elif x.endswith(".m2v"):
 					contents.append(".m2v")
-					print x+" is in."
+					print(x+" is in.")
 				elif x.endswith(".ts"):
 					contents.append(".ts")
-					print x+" will not be deleted for now."
+					print(x+" will not be deleted for now.")
 				else:
 					os.remove("/tmp/"+x)
-					print x+" removed."
-		print "We have got the following files:"
-		print contents
+					print(x+" removed.")
+		print("We have got the following files:")
+		print(contents)
 		if contents.count(".m2v") != 0:
 			if contents.count(".ac3") != 0:
 				if subprocess.Popen(["mplex","-f","3","-o","/tmp/"+movie+".mpg","/tmp/"+movie+".ac3","/tmp/"+movie+".m2v"]).wait() != 0: #threading?
@@ -110,14 +122,14 @@ class Movies2HDD:
 				if subprocess.Popen(["mplex","-f","3","-o","/tmp/"+movie+".mpg","/tmp/"+movie+".mp2","/tmp/"+movie+".m2v"]).wait() != 0: #threading?
 					raise BaseException
 			else:
-				print "No audio in here."
+				print("No audio in here.")
 				#return False
 				raise BaseException
 		else:
-			print "No video in here."
+			print("No video in here.")
 			#return False
 			raise BaseException
 		for x in contents:
-			print "deleting "+movie+x+"..."
+			print("deleting "+movie+x+"...")
 			os.remove("/tmp/"+movie+x)
 
