c17483b8af4f6ab3e38f3975e65d2019c689591d
Niklas Sombert <niklas1b@yahoo.de>
2013-10-20 01:18:56 +0200

    CLI: Starting to implement the parsing of the args...

--- cli.py
+++ cli.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 """A CLI to Movies2HDD"""
-print("TODO") #Not very helpful
+print("Loading libraries...") #Not very helpful
 from movies2hdd import Movies2HDD
 movies2hdd = Movies2HDD()
 #implement a command line parser
+import argparse
+
+
+parser = argparse.ArgumentParser(description="A command line interface to Movies2HDD.")
+parser.add_argument("command", metavar="command", type=str, help="The command to execute.")
+args = parser.parse_args()
+print (args)
