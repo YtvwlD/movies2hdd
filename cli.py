7cdc3077da15a688dbc7ab335cba85c988ce31a6
Niklas Sombert <niklas1b@yahoo.de>
2013-10-26 23:57:52 +0200

    Perhaps it will be a CLI.

--- cli.py
+++ cli.py
@@ -10,4 +10,22 @@ import argparse
 parser = argparse.ArgumentParser(description="A command line interface to Movies2HDD.")
 parser.add_argument("command", metavar="command", type=str, help="The command to execute.")
 args = parser.parse_args()
-print (args)
+#print (args.command)
+
+if args.command == "help":
+	print("The aviable commands are:")
+	print(" * convert [path to movie]	Converts the movie from .ts to .mpg. It removes additional audio tracks!")
+	print(" * ")
+
+if args.command == "convert":
+	import sys
+	import shutil
+	movie = sys.argv[1]
+	print("Moving "+movie+" to /tmp ...")
+	shutil.move(movie, "/tmp/")
+	print("Converting it...")
+	movie = movie.replace(".ts", "")
+	Movies2HDD.convertMovie(Movies2HDD(), movie)
+	print("Moving "+movie+".mpg back...")
+	shutil.move("/tmp/"+movie+".mpg", ".")
+
