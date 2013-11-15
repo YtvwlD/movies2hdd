1b84fd8bf94cef8de1f5fee9a180b04e8c037cc7
Niklas Sombert <niklas1b@yahoo.de>
2013-11-15 14:52:21 +0100

    Added a line-based interface. Well, almost.

---
+++ text.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''A line-based interface to Movies2HDD.'''
+
+print ("Movies2HDD's line-based interface Copyright (C) 2013 Niklas Sombert")
+print ("This program comes with ABSOLUTELY NO WARRANTY.")
+print ("This is free software, and you are welcome to redistribute it")
+print ("under certain conditions.")
